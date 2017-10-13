2b8eef5ef1c9f75c3ba3f9d3ad38881c6b2ca6f8
mail2yonz <mail2yonz@gmail.com>
2017-10-13 10:09:38 -0400

    Last commit of the fizz buzz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,8 @@ public class Main {
     public static void main(String[] args) {
 	// Creating a scanner object named scan
         Scanner scan= new Scanner(System.in);
-        int number,CountOfMultipleofSeven=0,CountOfMultipleOfSeventeen=0,CountOfMultipleOfTwentyThree=0;
+        int number,anotherNumber,CountOfMultipleofSeven=0;
+        int CountOfMultipleOfTheOtherNumber=0,CountOfMultipleOfSeventeen=0,CountOfMultipleOfTwentyThree=0;
 
  //do while condition that validates if the number is greater or equal to or grater than one
         do
@@ -25,6 +26,12 @@ public class Main {
             number= scan.nextInt();
             scan.nextLine();
             System.out.println();
+
+            //Askes the user to enter another number to check if it is multiple of the first number
+            System.out.println ("Enter Another Number To Check if it is multiple of the FirstNumber:" );
+            anotherNumber=scan.nextInt ();
+            scan.nextLine ();
+            System.out.println ( );
 
 
             //For loop that starts from one up to the number entered
@@ -53,6 +60,9 @@ public class Main {
                 //counts  the multiple of twenty three
                 if(i%17==0)
                     CountOfMultipleOfTwentyThree=CountOfMultipleOfTwentyThree+1;
+                //counts  the multiple of twenty three
+                if(i%anotherNumber==0)
+                    CountOfMultipleOfTheOtherNumber=CountOfMultipleOfTheOtherNumber+1;
 
             }
             //Displays the result
@@ -60,6 +70,7 @@ public class Main {
             System.out.println("The number of Count of Multiple of 7 is : "+CountOfMultipleofSeven);
             System.out.println("The number of Count of Multiple of 17 is : "+CountOfMultipleOfSeventeen);
             System.out.println("The number of Count of Multiple of 23 is : "+CountOfMultipleOfTwentyThree);
+            System.out.println("The number of Count of Multiple of 23 is : "+CountOfMultipleOfTheOtherNumber);
             System.out.println();
 
             //if the number is less than or equals to one asks the user to enter a number grater than one
